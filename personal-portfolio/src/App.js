5aba30fa8cf69f1f893435c2d84ba6e99e533a82
ryodan95 <sk8.poggi95@gmail.com>
2023-08-28 12:12:09 -0300

    1.1

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -11,11 +11,7 @@ import Contact from "./components/Contact";
 
 
 function App() {
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => { console.log (data)})
-  }, [])
+
 
   return (
     <div className="App">
